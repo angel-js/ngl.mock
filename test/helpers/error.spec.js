967ba1ace98bb2731eaa8d0e49c9914440e8af2b
Pau Fracés <pfraces@gmail.com>
2016-04-17 16:28:36 +0200

    done error test coverage

--- test/helpers/error.spec.js
+++ test/helpers/error.spec.js
@@ -7,4 +7,16 @@ describe('error', function () {
   it('should expose a function', function () {
     expect(error).to.be.a('function');
   });
+
+  it('should return a `new Error`', function () {
+    expect(error()).to.be.an(Error);
+  });
+
+  it('should create an error with the provided message', function () {
+    expect(error('foo').message).to.be('foo');
+  });
+
+  it('should join with spaces multiple arguments', function () {
+    expect(error('foo', 'bar', 'qux').message).to.be('foo bar qux');
+  });
 });
