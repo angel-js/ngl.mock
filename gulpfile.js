c83b0713d890fa8bd1a749f8da0a3de8fcac13f2
Pau Fracés <pfraces@gmail.com>
2016-04-17 21:25:37 +0200

    added istambul test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var del = require('del');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('clean', function () {
   return del(['tmp', 'dist']);
@@ -19,9 +20,21 @@ gulp.task('lint', ['clean'], function () {
     .pipe(eslint.failOnError());
 });
 
-gulp.task('test', ['lint'], function () {
+gulp.task('istambul-hook-require', ['lint'], function () {
+  return gulp.src('src/**/*.js')
+    .pipe(istanbul({ includeUntested: true }))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['istambul-hook-require'], function () {
   return gulp.src('test/**/*.js')
-    .pipe(mocha());
+    .pipe(mocha())
+    .pipe(istanbul.writeReports({
+      dir: 'tmp/coverage',
+      reporters: ['lcov', 'text-summary'],
+      reportOpts: { dir: 'tmp/coverage' }
+    }))
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 80 } }));
 });
 
 gulp.task('scripts', ['test'], function () {
